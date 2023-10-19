7ffcad9fe7ce895efb587479e06198289a48a460
Millieme <wcnsk15@gmail.com>
2023-10-19 23:27:10 +0700

    Test Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,10 @@ import Sf from '../components/sf.vue'
 </script>
 
 <template><br>
-    <SearchFilter /> <br>
+    <!-- <SearchFilter /> <br> -->
     <!-- <Sf /> -->
     <!-- v-if data===0 -->
 
-      <p class="text-center text-lg mt-10">No Stray animals now</p>
+      <p class="text-center text-lg mt-10">Test Home Page</p>
 
 </template>
