cff257728108d6c46dc5536c0c7a3f5c5440d079
punchsppt <supapit.r@mail.kmutt.ac.th>
2023-11-30 22:49:19 +0700

    accept http and https test

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         target: 'http://54.179.85.138', // http to VM's IP
         rewrite: path => path.replace(/^\/api/, ''),
       changeOrigin: true,
-      secure: false, // Set this flag to false to allow HTTP requests
+      secure: false, // This line allows usage of self-signed SSL certificates
       }
     },
     port: 2304, 
