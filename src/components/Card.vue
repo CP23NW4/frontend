031f6a9700b228f0762d4d1c7b3fccbeebb85864
punchsppt <supapit.r@mail.kmutt.ac.th>
2023-12-07 16:03:56 +0700

    comment animal pictures

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -18,11 +18,11 @@ console.log(id)
 <template>
 <!-- {{ strayAnimal.id }} -->
   <div @click="showDetail(strayAnimal.id)" class="mx-12 mb-10 text-left block max-w-sm bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-lg transition-transform transform hover:-translate-y-1 focus:translate-y-0 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <div class="relative w-full h-52">
+    <!-- <div class="relative w-full h-52">
         <img v-if="strayAnimal.picture !== null" :src="strayAnimal.picture" class="w-full h-full object-cover rounded-lg" alt="Animal Image">
         <img v-else-if="strayAnimal.picture === null" src="/nodata.png" class="w-full h-full object-cover rounded-lg" alt="Animal Image">
-
-    </div>
+    </div> -->
+    
     <div class="flex items-center p-5">
       <img v-if="strayAnimal.gender === 'Female'" src="/female.svg" class="h-5 mr-4" alt="Female Icon">
       <img v-if="strayAnimal.gender === 'Male'" src="/male.svg" class="h-5 mr-4" alt="Male Icon">
