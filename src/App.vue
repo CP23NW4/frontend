20207926eecc6e208d414582b0e2db0b9971b428
punchsppt <supapit.r@mail.kmutt.ac.th>
2024-03-17 19:36:40 +0700

    CREATE: new env file for handling prod path

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
- <script setup>
-import Navbar from "./bar/Navbar.vue";
-import Footer from './bar/Footer.vue';
-
-import { useRouter } from "vue-router";
-// const router = useRouter();
-// const clearConsole = () => {
-//   console.clear();
-// };
-
-// router.afterEach(() => {
-//   clearConsole();
-// });
-
- </script>
-
 <template>
-  <div> 
-   <Navbar />
-  <router-view class="min-h-screen" />
-  <!-- <Footer /> -->
+  <div>
+    <Navbar />
+    <router-view class="min-h-screen" />
+    <!-- <Footer /> -->
+    <!-- <span>Version: {{ version }}</span>
+    <span>path: {{ path }}</span> -->
   </div>
 </template>
 
+<script setup>
+import Navbar from './bar/Navbar.vue'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
 
-
+// Accessing environment variables
+// const version = import.meta.env.VITE_APP_VERSION
+// const path = import.meta.env.VITE_APP_TITLE
+</script>
